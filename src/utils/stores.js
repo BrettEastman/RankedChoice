eebd3dedf210300ae4814c0d3e27f2888b78dc49
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-07-08 17:46:07 -0700

    Refactor store to tweened. Delete separate incrementers/decrementers/resetters and include everything in App.svelte

--- src/utils/stores.js
+++ src/utils/stores.js
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store';
 
-export const candidateCount = writable(0);
-export const voterCount = writable(0);
+// export const candidateCount = writable(0);
+// export const voterCount = writable(0);
