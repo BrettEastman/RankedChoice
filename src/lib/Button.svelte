eb39b499b37d5db46ae09ed8aaa4881afae95f6e
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-10-31 23:46:12 -0700

    Refactor Stack and Button to include addClass attribute, then updated documentation. Refactored App to add transition durations to all buttons in Tailwind and standard CSS.

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,24 +1,25 @@
 <script>
   export let color = "bg-primary hover:bg-primaryDark";
+  export let addClass = "";
 
   export let onClick = function () {
     // Click handler goes here
   };
+
+  const className = `${addClass} rounded-full px-3 py-1 tracking-wide text-white font-bold text-sm border-solid border-1 shadow-[rgba(0,_0,_0,_0.24)_0px_2px_2px] ${color} active:bg-primaryDark`;
 </script>
 
 <!--
 @component
 Button is a styled button component that can be used to create a button with a background color and a shadow. Adapted from component library originally developed by Brett Eastman and Alex Nitta.
 
-| Prop    | Type     | Options                     | Default                            | Description                                                                                      |
-| ------- | -------- | --------------------------- | ---------------------------------- | ------------------------------------------------------------------------------------------------ |
-| color   | string   | Any Tailwind bg color class | 'bg-primary hover:bg-primaryDark'  | Indicates the background color of the button. You can optionally include diffrent color on hover |
-| onClick | function |                             | -                                  | Callback invoked when button is clicked                                                          |
+| Prop     | Type     | Options                     | Default                            | Description                                                                                      |
+| -------- | -------- | --------------------------- | ---------------------------------- | ------------------------------------------------------------------------------------------------ |
+| color    | string   | Any Tailwind bg color class | 'bg-primary hover:bg-primaryDark'  | Indicates the background color of the button. You can optionally include diffrent color on hover |
+| addClass | string   | Any Tailwind/CSS class      | ''                                 | Can be any Tailwind class name or standard CSS class name                                        |
+| onClick  | function |                             | -                                  | Callback invoked when button is clicked                                                          |
 -->
 
-<button
-  class="rounded-full px-3 py-1 tracking-wide text-white font-bold text-sm border-solid border-1 shadow-[rgba(0,_0,_0,_0.24)_0px_2px_2px] {color} active:bg-primaryDark"
-  on:click={onClick}
->
+<button class={className} on:click={onClick}>
   <slot />
 </button>
--- src/lib/Stack.svelte
+++ src/lib/Stack.svelte
@@ -1,16 +1,19 @@
 <script>
-  export let gutter = 'gap-2';
+  export let gutter = "gap-2";
+  export let addClass = "";
+  const className = `${addClass} ${gutter}`;
 </script>
 
 <!--
 @component
-Stack is a layout component designed to 'stack' items on top of each other while maintaining a consistent gutter between each item. Originally developed by Brett Eastman and Alex Nitta.
+Stack is a layout component designed to 'stack' items on top of each other while maintaining a consistent gutter between each item. Adapted from component library originally developed by Brett Eastman and Alex Nitta.
 
-| Prop   | Type   | Options                             | Default | Description                           |
-| ------ | ------ | ----------------------------------- | ------- | ------------------------------------- |
-| gutter | string | Tailwind class name such as 'gap-4' | 'gap-2' | Used to set the spacing between items |
+| Prop     | Type   | Options                             | Default | Description                                               |
+| -------- | ------ | ----------------------------------- | ------- | --------------------------------------------------------- |
+| gutter   | string | Tailwind class name such as 'gap-4' | 'gap-2' | Used to set the spacing between items                     |
+| addClass | string | Any Tailwind/CSS class              | ''      | Can be any Tailwind class name or standard CSS class name |
 -->
 
-<div class="flex flex-col {gutter}">
+<div class="flex flex-col {className}">
   <slot />
 </div>
